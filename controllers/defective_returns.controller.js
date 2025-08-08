f35f9c06c396013971ad0e6734be806b864fb471
Abdel Kader Diarra <abdelkaderdiarra707@gmail.com>
2025-08-08 17:24:29 +0000

    Affichage des comptes des mobiles

--- controllers/defective_returns.controller.js
+++ controllers/defective_returns.controller.js
@@ -10,12 +10,19 @@ exports.getAllDefectiveReturns = async (req, res) => {
         dr.reason,
         dr.return_date,
         dr.status,
+        c.nom AS client_nom,
+        c.telephone AS client_telephone,
         p.marque,
         p.modele,
         p.stockage,
-        p.type
+        p.type,
+        p.type_carton
       FROM defective_returns dr
-      JOIN products p ON dr.product_id = p.id
+      LEFT JOIN products p ON dr.product_id = p.id
+      LEFT JOIN vente_items vi ON p.id = vi.product_id
+      LEFT JOIN ventes v ON vi.vente_id = v.id
+      LEFT JOIN clients c ON v.client_id = c.id
+      GROUP BY dr.id, c.id, p.id, vi.id
       ORDER BY dr.return_date DESC
     `);
     res.status(200).json(returns.rows);
